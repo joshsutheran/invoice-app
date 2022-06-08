c89f4c97860dfc4232415fbeaec3317db7b6a549
Josh Sutheran <josh.sutheran@gmail.com>
2022-06-08 22:44:54 +0100

    built filter tool

--- src/components/invoiceList/invoiceList.jsx
+++ src/components/invoiceList/invoiceList.jsx
@@ -1,9 +1,24 @@
 import images from "../../constants/images/images";
 import FilterBox from "../filterBox/filterBox";
 
+import { useState } from "react";
+
 import "./invoiceList.css";
 
 const InvoiceList = (props) => {
+  const [checkedPaid, setCheckedPaid] = useState(false)
+  const [checkedPending, setCheckedPending] = useState(false)
+  const [checkedDraft, setCheckedDraft] = useState(false)
+
+  function filterChange(event) {
+    if (event.currentTarget.id === "filter-paid") {
+      setCheckedPaid(!checkedPaid)
+    } else if (event.currentTarget.id === 'filter-pending') {
+      setCheckedPending(!checkedPending)
+    } else if (event.currentTarget.id === 'filter-draft') {
+      setCheckedDraft(!checkedDraft)
+    }
+  }
   return (
     <div className="invoices__wrapper">
       <div className="invoices">
@@ -23,30 +38,91 @@ const InvoiceList = (props) => {
             <p>New Invoice</p>
           </div>
         </div>
-        <FilterBox filter={props.filterActive} />
+        <FilterBox
+          filterChange={filterChange}
+          filterPaid = {checkedPaid}
+          filterPending = {checkedPending}
+          filterDraft = {checkedDraft}
+          filter={props.filterActive}
+        />
         <div className="invoice__list">
-          {props.invoices.map((invoice, index) => {
-              let date = new Date(invoice.paymentDue).toString().split('').slice(0, 15).join('')
-              let status = invoice.status.charAt(0).toUpperCase() + invoice.status.slice(1)
-              return (
-                <div key={index} className="invoice">
+          {
+            props.invoices.filter((invoice) => {
+              if (checkedPaid === false && checkedPending === false && checkedDraft === false) {
+                return invoice
+              } else if ((checkedPaid && checkedPending)) {
+                return invoice.status === 'paid' || invoice.status === 'pending'
+              } else if ((checkedPaid && checkedDraft)) {
+                return invoice.status === 'paid' || invoice.status === 'draft'
+              } else if(checkedPending && checkedDraft) {
+                return invoice.status === 'draft' || invoice.status === 'pending'
+              } else if (checkedPaid) {
+                return invoice.status === 'paid'
+              } else if (checkedPending) {
+                return invoice.status === 'pending'
+              } else if (checkedDraft) {
+                return invoice.status === 'draft'
+              }
+            })
+            .map((invoice, index) => {
+            let date = new Date(invoice.paymentDue)
+              .toString()
+              .split("")
+              .slice(0, 15)
+              .join("");
+            let status =
+              invoice.status.charAt(0).toUpperCase() + invoice.status.slice(1);
+            return (
+              <div key={index} className="invoice">
                 <div className="invoice__list-left">
                   <h4>
-                    <span className="hashtag">#</span>{invoice.id}
+                    <span className="hashtag">#</span>
+                    {invoice.id}
                   </h4>
-                  <p>Due: {(date)}</p>
+                  <p>Due: {date}</p>
                 </div>
                 <div className="invoice__list-right">
-                  <div style={{'backgroundColor': invoice.status === 'paid' ? "rgb(0,255,0, 0.08)" : invoice.status === 'pending' ? 'rgba(255, 143, 0, 0.06)' : 'rgba(55, 59, 83, 0.06)'}} className="invoice__list-btn">
-                    <div style={{'backgroundColor': invoice.status === 'paid' ? "rgb(0,100,0)" : invoice.status === 'pending' ? 'rgba(255, 143, 0)' : 'rgba(55, 59, 83)'}} className="dot"></div>
-                    <p style={{'color': invoice.status === 'paid' ? "rgb(0,100,0)" : invoice.status === 'pending' ? 'rgba(255, 143, 0)' : 'rgba(55, 59, 83)'}}>{status}</p>
+                  <div
+                    style={{
+                      backgroundColor:
+                        invoice.status === "paid"
+                          ? "rgb(0,255,0, 0.08)"
+                          : invoice.status === "pending"
+                          ? "rgba(255, 143, 0, 0.06)"
+                          : "rgba(55, 59, 83, 0.06)",
+                    }}
+                    className="invoice__list-btn"
+                  >
+                    <div
+                      style={{
+                        backgroundColor:
+                          invoice.status === "paid"
+                            ? "rgb(0,100,0)"
+                            : invoice.status === "pending"
+                            ? "rgba(255, 143, 0)"
+                            : "rgba(55, 59, 83)",
+                      }}
+                      className="dot"
+                    ></div>
+                    <p
+                      style={{
+                        color:
+                          invoice.status === "paid"
+                            ? "rgb(0,100,0)"
+                            : invoice.status === "pending"
+                            ? "rgba(255, 143, 0)"
+                            : "rgba(55, 59, 83)",
+                      }}
+                    >
+                      {status}
+                    </p>
                   </div>
                   <div className="invoice__list-arrow">
                     <img src={images.rightArrow} alt="Right Arrow" />
                   </div>
                 </div>
               </div>
-              )
+            );
           })}
         </div>
       </div>
